54d6c58c421336e509bd9261c58d18c1d6ce7544
tepmehatop <tepmehatop@hotmail.com>
2018-11-07 00:01:53 +0300

    Введение в Java и JUnit / ► Тест 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -11,4 +11,18 @@ public class MainClassTest {
         Assert.assertEquals("getLocalNumber возвращает число не равное 14",14,b);
     }
 
+
+    @Test
+    public void testGetClassNumber()
+    {
+        MainClass mainClass = new MainClass();
+        int a = mainClass.getClassNumber();
+        int c = 45;
+
+        Assert.assertTrue("getClassNumber возвращает число меньше 45", a > c);
+
+    }
+
+
+
 }
